78970bbe8a0c3783860bcc508f6b335450716af4
NEW SPORT <gashaw7abi@gmail.com>
2026-08-19 20:40:53 +0300

    refactor: update logo path to use public directory

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ import React, { useState, useEffect } from "react";
 import { signInWithPopup, signOut, onAuthStateChanged, User } from "firebase/auth";
 import { collection, addDoc, serverTimestamp, getDocs, orderBy, limit, query, doc, setDoc, increment, getDoc, deleteDoc } from "firebase/firestore";
 import { auth, googleProvider, db } from "./firebase";
-import techLogo from "./assets/tech-logo-v2.png";
+const techLogo = "/tech-logo.png";
 
 interface NewsItem {
   id: string | number;
